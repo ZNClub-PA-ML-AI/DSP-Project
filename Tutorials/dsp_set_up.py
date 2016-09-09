0ad9f8fd54751002f542581749b8996b8a1980f5
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-09-09 10:25:59 +0530

    method added

--- Tutorials/dsp_set_up.py
+++ Tutorials/dsp_set_up.py
@@ -7,23 +7,33 @@ Created on Fri Sep  9 09:15:24 2016
 import wave
 print("import success")
 
+### method to recognize a YES or a NO from wav sample
+def recognize_yes_or_no(x,fs):
+    print("inside f(x)")
+    return
+
 ### read a wav file
 with wave.open("test1.wav", mode='rb') as wav:
     print("read success")
+#    
+#    # does not work
+#    print(wav)
+#    
+#    print("Returns number of audio channels (1 for mono, 2 for stereo).\n",wav.getnchannels())
+#    
+#    print("Returns sample width in bytes.\n",wav.getsampwidth())
+#    
+#    print("Returns sampling frequency.\n",wav.getframerate())
+#
+#    print("Returns number of audio frames.\n",wav.getnframes())
+#    
+#    print("Returns compression type \n",wav.getcomptype(),wav.getcompname())    
+#
+#    print("Returns a namedtuple() (nchannels, sampwidth, framerate, nframes, comptype, compname), equivalent to output of the get*() methods\n",wav.getparams())    
+#    
+    recognize_yes_or_no(wav.getnframes,wav.getframerate)    
     
-    # does not work
-    print(wav)
-    
-    print("Returns number of audio channels (1 for mono, 2 for stereo).\n",wav.getnchannels())
-    
-    print("Returns sample width in bytes.\n",wav.getsampwidth())
-    
-    print("Returns sampling frequency.\n",wav.getframerate())
-
-    print("Returns number of audio frames.\n",wav.getnframes())
-    
-    print("Returns compression type \n",wav.getcomptype(),wav.getcompname())    
-
-    print("Returns a namedtuple() (nchannels, sampwidth, framerate, nframes, comptype, compname), equivalent to output of the get*() methods\n",wav.getparams())    
     wav.close()
     print("close success")
+    
+    
